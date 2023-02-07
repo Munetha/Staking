984f73ddcc96bfeef3aeb59306a720dd7de8a1eb
sabir aboobaker <sabir.aboobaker@accubits.com>
2023-02-07 18:15:43 +0530

    added packages

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,17 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-web3");
+require("@nomiclabs/hardhat-waffle");
+require("ethereum-waffle");
+require("@openzeppelin/hardhat-upgrades");
+require("@nomiclabs/hardhat-ethers");
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
