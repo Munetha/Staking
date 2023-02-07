3fcf0eb4a9a973dea10af614d4e679f1917774bd
sabir aboobaker <sabir.aboobaker@accubits.com>
2023-02-07 18:18:52 +0530

    code formated

--- test/test.js
+++ test/test.js
@@ -4,35 +4,35 @@ const { ethers } = require("hardhat");
 describe('Staking', () => {
     let deployer;
     let staker;
-    
-    let staking ;
-let erc20 ;
-let erc721 ;
-let demoErc20 ;
 
-const deployStaking = async() => {
-    const StakingFactory = await ethers.getContractFactory('Staking');
-    const Staking = await StakingFactory.deploy();
-    staking = await Staking.deployed();
-};
+    let staking;
+    let erc20;
+    let erc721;
+    let demoErc20;
 
-const deployErc20 = async() => {
-    const RewardTokenFactory = await ethers.getContractFactory('RewardToken');
-    const RewardToken = await RewardTokenFactory.deploy(staking.address);
-    erc20 = await RewardToken.deployed();
-};
+    const deployStaking = async () => {
+        const StakingFactory = await ethers.getContractFactory('Staking');
+        const Staking = await StakingFactory.deploy();
+        staking = await Staking.deployed();
+    };
 
-const deployErc721 = async() => {
-    const RewardNFTFactory = await ethers.getContractFactory('RewardNFT');
-    const RewardNFT = await RewardNFTFactory.deploy(staking.address);
-    erc721 = await RewardNFT.deployed();
-};
+    const deployErc20 = async () => {
+        const RewardTokenFactory = await ethers.getContractFactory('RewardToken');
+        const RewardToken = await RewardTokenFactory.deploy(staking.address);
+        erc20 = await RewardToken.deployed();
+    };
 
-const deployDemo = async() => {
-    const DemoFactory = await ethers.getContractFactory('Demo');
-    const Demo = await DemoFactory.deploy();
-    demoErc20 = await Demo.deployed();
-};
+    const deployErc721 = async () => {
+        const RewardNFTFactory = await ethers.getContractFactory('RewardNFT');
+        const RewardNFT = await RewardNFTFactory.deploy(staking.address);
+        erc721 = await RewardNFT.deployed();
+    };
+
+    const deployDemo = async () => {
+        const DemoFactory = await ethers.getContractFactory('Demo');
+        const Demo = await DemoFactory.deploy();
+        demoErc20 = await Demo.deployed();
+    };
 
     before(async () => {
         signers = await ethers.getSigners();
@@ -47,34 +47,34 @@ const deployDemo = async() => {
     });
 
     it('set reward contract address in staking', async () => {
-       
+
     });
 
     it('mint demo token and set allowance', async () => {
-       
+
     });
 
     it('deposit token', async () => {
-       
+
     });
 
     it('check staking status', async () => {
-       
+
     });
 
     it('withdraw token', async () => {
-       
+
     });
 
     it('check staking status after withdraw', async () => {
-       
+
     });
 
     it('clain reward', async () => {
-       
+
     });
 
     it('check staking status after claim', async () => {
-       
+
     });
 });
