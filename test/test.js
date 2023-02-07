9dceb81bc8cbb16226131c3babc2103c2fbddd52
Munetha <84306114+Munetha@users.noreply.github.com>
2023-02-07 15:31:33 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,37 +1,3 @@
-// import { ethers } from 'hardhat';
-
-// const deployNiftySouq = async (deployer: any, serviceFee: number, bidIncreasePercentage: number) => {
-//     const NiftySouqFactory = await ethers.getContractFactory('NiftySouq');
-//     const niftySouq = (await NiftySouqFactory.deploy());
-//     await niftySouq.deployed();
-//     await niftySouq.connect(deployer).initialize(serviceFee, bidIncreasePercentage);
-
-//     return niftySouq;
-// };
-// describe('NiftySouq - Fixed Price Sale with Cypto Currency', () => {
-//     let niftySouq: any;
-//     let signers: any;
-
-//     let owner: any;
-//     let minter: any;
-//     before(async () => {
-//         signers = await ethers.getSigners();
-
-//         owner = signers[0];
-//         minter = signers[1];
-
-//         niftySouq = await deployNiftySouq(owner, 0, 0);
-//     });
-
-//     it('mint token to owner', async () => {
-//         await niftySouq.connect(minter)['mint(uint256,(uint256[],address[]))'](1, {
-//             royalties: [50, 50],
-//             creators: ['dsfgdfg', 'sdfgdfsgdfgf']
-//         });
-//     });
-// });
-
-
 import { ethers } from 'hardhat';
 const deployStaking = async() => {
     const StakingFactory = await ethers.getContractFactory('Staking');
