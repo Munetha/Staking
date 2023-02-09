f057c4b21a649886e635fba20bff0fbcd6282953
Munetha <84306114+Munetha@users.noreply.github.com>
2023-02-09 15:53:16 +0530

    Update Staking.sol

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "hardhat/console.sol";
-import { time } from "@nomicfoundation/hardhat-network-helpers";
 
 contract Staking {
     address public Erc20;
