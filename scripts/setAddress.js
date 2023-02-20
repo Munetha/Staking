0f440544a00eec0faa7d2ff809fe7500be7580cc
Arun <arunachalamani.g@accubits.com>
2023-02-20 10:07:02 +0530

    scripts updated

--- scripts/setAddress.js
+++ scripts/setAddress.js
@@ -1,22 +1,18 @@
-const { ethers } = require("hardhat");
+const hre = require("hardhat");
+
 
 async function main() {
   // Set up the provider and signer
-  const signer = (await ethers.getSigners())[0];
+  const [admin] = await hre.ethers.getSigners();
 
   // Deploy the contract
-  const Staking = await ethers.getContractFactory("Staking");
-  const staking = await Staking.attach("0xD33fFb67a691135bcA891959A83533fF098cC494");
-
+  const Staking = await hre.ethers.getContractFactory("Staking");
+  const staking = await Staking.deploy();
   // Call the setAdresses function
   const erc20Address = "0x8E65d8F4875272955891469093f5730E6b6E8cbf"; // Replace with the address of your ERC20 token
   const nftAddress = "0xfb46d32822bbEA26e2B62e377c38BE777B06c316"; // Replace with the address of your NFT
-  const tx = await staking.connect(signer).setAdresses(erc20Address, nftAddress);
+  const tx = await staking.connect(admin).setAdresses(erc20Address, nftAddress);
   console.log("Transaction hash:", tx.hash);
-
-
-  console.log("RewardToken is :", staking.ERC20Reward());
-  console.log("RewardNFT is :", staking.ERC721NFT());
 
 }
 
